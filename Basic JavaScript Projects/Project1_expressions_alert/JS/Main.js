0d828f9f250e3c9490a96489a482395ce2d86e41
wbennett84 <81777304+wbennett84@users.noreply.github.com>
2021-05-20 20:01:55 -0400

    commit first assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,35 @@
+document.write("Hello, World!<br>");
+
+var A = "This is a string<br>";
+document.write(A);
+
+var B = "Hot Diggity Dog!";
+window.alert(B);
+
+document.write("I got the pickles. Aren\'t you happy about that? He replied \"No.\"<br>Hello Cat.");
+
+document.write("\"He was a natural born football player.\" said Bobby "
++"who never much said anything.<br><br>");
+
+
+
+var C = "Concatenated" + " String";
+var CC = " Theory.<br><br>";
+document.write(C+CC)
+
+
+var Family = "The Bennetts", Dad = "Will", Mom = "Diliana", Daughter = "Amber", Son = "Azure";
+var Family = Family.fontcolor("blue");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("yellow");
+var Daughter = Daughter.fontcolor("purple");
+var Son = Son.fontcolor("green");
+document.write(Dad);
+
+
+var blues = "<br><br>I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+
+document.write(4+5);
