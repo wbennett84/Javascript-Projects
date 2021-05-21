1ed54d62d1ed472df03a49ba2bab5050ae815ad9
wbennett84 <81777304+wbennett84@users.noreply.github.com>
2021-05-21 17:47:44 -0400

    Commit Assignment Update

--- Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -1,22 +1,27 @@
+// This writes Hello World
 document.write("Hello, World!<br>");
 
+
+// Sets variable and tells to write it on page
 var A = "This is a string<br>";
 document.write(A);
-
+// Create an alert box
 var B = "Hot Diggity Dog!";
 window.alert(B);
-
+//Writes some text
 document.write("I got the pickles. Aren\'t you happy about that? He replied \"No.\"<br>Hello Cat.");
 
 document.write("\"He was a natural born football player.\" said Bobby "
 +"who never much said anything.<br><br>");
 
 
-
+//Sets up some variables and links some text and writes it on screen
 var C = "Concatenated" + " String";
 var CC = " Theory.<br><br>";
 document.write(C+CC)
 
+
+//Sets a bunch of variables and assigns them all font color
 
 var Family = "The Bennetts", Dad = "Will", Mom = "Diliana", Daughter = "Amber", Son = "Azure";
 var Family = Family.fontcolor("blue");
@@ -26,10 +31,12 @@ var Daughter = Daughter.fontcolor("purple");
 var Son = Son.fontcolor("green");
 document.write(Dad);
 
+//Sets variables and assigns font color and writes text
 
 var blues = "<br><br>I have the blues.";
 var blues = blues.fontcolor("blue");
 document.write(blues);
 
+//writes a sum figure
 
 document.write(4+5);
