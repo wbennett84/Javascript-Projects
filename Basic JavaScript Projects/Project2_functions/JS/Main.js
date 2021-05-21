1a43e195f1ddc5e8e8c7d2d514aa8d323447c8c6
wbennett84 <81777304+wbennett84@users.noreply.github.com>
2021-05-20 20:54:30 -0400

    Commit Next Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/Main.js
@@ -0,0 +1,8 @@
+
+
+// This is a function which connects two strings and displays them in the linked HTML document.
+function myFunction() {
+    var sentence = "I am William Matthew Bennett";
+    sentence += " and I'm having a groovy time!";
+    document.getElementById("second").innerHTML = sentence;
+}
